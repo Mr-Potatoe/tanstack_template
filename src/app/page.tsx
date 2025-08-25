759297efeb441c35ce0fa4ce5a8e58f9ee906ba4
Mr-Potatoe <jaymeajarns@gmail.com>
2025-08-25 19:29:40 +0800

    first commit for reusable template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ThemeSwitch } from "@/components/theme/switch";
 import Image from "next/image";
 
 export default function Home() {
@@ -96,6 +97,15 @@ export default function Home() {
             height={16}
           />
           Go to nextjs.org →
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <ThemeSwitch />
+          Theme Switch
         </a>
       </footer>
     </div>
